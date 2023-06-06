7eebf685769252bb7ca39b7324d2ec570a495bbf
sabrynaElkind <sabryna.elkind11@gmail.com>
2023-06-06 19:17:49 +0200

    Fixed

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,25 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, linkToFirst = 1;
+        int second = 10, linkToSecond = 10;
+        int third = 100, linkToThird = 100;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
